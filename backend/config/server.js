2d3267136005c9eab954535d19897aee563f54ae
Cai305 <34400593+Cai305@users.noreply.github.com>
2020-09-05 12:26:35 +0200

    setup

--- backend/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '26102f51ce3ea6e0d334043907fd8286'),
-    },
-  },
-});
--- backend/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '7836621e-6fb6-4aff-a1ca-039158204d60'
-};
